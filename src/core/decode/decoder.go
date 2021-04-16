25c75f174f1967d241251050a291b1f2f0c8ac4f
Michał Kowal <michalkowal567@gmail.com>
2021-04-16 12:53:49 +0200

    Clean solution for makeover of the code

--- src/core/decode/decoder.go
+++
@@ -1,8 +0,0 @@
-package decode
-
-import "core"
-
-//decoder is responsible for implementing of decode method which returns core.Form object for further usage.
-type decoder interface {
-	Decode(linesChannel chan string) *core.Form
-}
--- src/main.go
+++ src/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"core/decode"
-	"core/read"
-)
-
 func main() {
-	mr := read.MarkdownReader{
-		ReadChannel: make(chan string, 1),
-	}
 
-	decoder := decode.Decoder{DecodingHandler: &decode.MarkdownDecoder{}}
-	go mr.ReadMarkdownContentToChannel("##hejka **XD** **lol**")
-	form := decoder.Decode(mr.ReadChannel)
-	println(form.Fields)
-	<- mr.ReadChannel
 }
